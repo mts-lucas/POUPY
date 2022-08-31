231f8b41c7772abbe69f4907701447c1d6c4b125
Lucas Mateus <lmateus1067@outlook.com>
2022-08-31 00:48:29 -0300

    num sei

--- classes.py
+++ classes.py
@@ -9,19 +9,53 @@ pygame.init()
 diretorio_principal = os.path.dirname(__file__)
 diretorio_imagens = os.path.join(diretorio_principal, 'sprites')
 diretorio_sons = os.path.join(diretorio_principal, 'trilha sonora')
-sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'link_sprites.png')).convert_alpha()
+sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'link_sprites.png'))
 
 #criando a classe do bichinho virtual
 
 class Poupy(pygame.sprite.Sprite):
 
     def __init__(self):
-        pygame.sprite.Sprite(self)
+        pygame.sprite.Sprite.__init__(self)
         self.imagem_poupy = []
-        for i in range():
-            img = 
-        self.img = sprite_sheet.subsurface((0,0), (120, 130))
+        for i in range(43):
+            img = sprite_sheet.subsurface((i * 120, 0), (120, 130))
+            self.imagem_poupy.append(img)
 
-    
-    def andar(self):
+        self.index_lista = 0
+        self.image = self.imagem_poupy[self.index_lista]
+        self.rect = self.image.get_rect()
+        self.rect.center = (300, 300)
+        self.andar_d = False
+        self.andar_a = False
+        self.andar_w = False
+        self.andar_s = False
+
+    def update(self):
+        if self.andar_d == True:
+            if self.index_lista >= 42:
+                self.index_lista = 33
+                self.andar_d = False
+
+            self.index_lista += 0.1
+        
+        else:
+            if self.index_lista > 2:
+                self.index_lista = 0
+
+            self.index_lista += 0.025
+        self.image = self.imagem_poupy[int(self.index_lista)]
+
+    def andar_direita(self):
+        self.andar_d = True
+        self.index_lista = 33
+
+    def andar_esquerda(self):
         pass
+
+    def andar_cima(self):
+        pass
+
+    def andar_baixo(self):
+        pass
+
--- main.py
+++ main.py
@@ -2,33 +2,42 @@ import pygame
 from pygame.locals import *
 from sys import exit
 import os
+from classes import Poupy
 
 pygame.init()
 
-#musica de fundo do jogo
+# musica de fundo do jogo
 
 pygame.mixer.music.set_volume(0.50)
 pygame.mixer.music.load("trilha sonora/BoxCat Games - Young Love.mp3")
 pygame.mixer.music.play(-1)
 
-#adcionando diretorios
+# adcionando diretorios
 
-diretorio_principal = os.path.dirname(__file__)
-diretorio_imagens = os.path.join(diretorio_principal, 'sprites')
-diretorio_sons = os.path.join(diretorio_principal, 'trilha sonora')
-sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'link_sprites.png')).convert_alpha()
+# diretorio_principal = os.path.dirname(__file__)
+# diretorio_imagens = os.path.join(diretorio_principal, 'sprites')
+# diretorio_sons = os.path.join(diretorio_principal, 'trilha sonora')
+# sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'link_sprites.png'))
 
 # trecho dedicado a janela do jogo
 
 largura_janela = 640
 altura_janela = 480
 
+#criando a classe do bixinho:
+
+
 
 tela_fundo = pygame.image.load('sprites/telafundo.png')
 tela_fundo = pygame.transform.scale(tela_fundo, (largura_janela, altura_janela))
+
 tela_jogo = pygame.display.set_mode((largura_janela, altura_janela))
 pygame.display.set_caption("Poupy")
 relogio_jogo = pygame.time.Clock()
+
+todas_as_sprites = pygame.sprite.Group()
+bixinho = Poupy()
+todas_as_sprites.add(bixinho)
 
 while True:
 
@@ -41,7 +50,12 @@ while True:
             pygame.quit()
             exit()
 
+        if pygame.key.get_pressed()[K_d]:
+            bixinho.andar_direita()
+
     tela_jogo.blit(tela_fundo, (0, 0))
+    todas_as_sprites.draw(tela_jogo)
+    todas_as_sprites.update()
     pygame.display.flip()
 
-#Fim do trecho da janela
+# Fim do trecho da janela
