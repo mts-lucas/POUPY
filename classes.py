d34f34dca6e1114a52546c0c9e4062feaeeda925
Lucas Mateus <lmateus1067@outlook.com>
2022-08-31 14:11:08 -0300

    coringuei

--- classes.py
+++ classes.py
@@ -17,38 +17,54 @@ class Poupy(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.imagem_poupy = []
+        #setando os sprites de cada direção
+        self.img_stoped = []
         for i in range(43):
             img = sprite_sheet.subsurface((i * 120, 0), (120, 130))
-            self.imagem_poupy.append(img)
+            self.img_stoped.append(img)
 
-        self.index_lista = 0
-        self.image = self.imagem_poupy[self.index_lista]
+        self.img_down = []
+        for j in range(3, 13):
+            img = sprite_sheet.subsurface((j * 120, 0), (120, 130))
+            self.img_down.append(img)
+        
+        self.img_left = []
+        for k in range(13, 23):
+            img = sprite_sheet.subsurface((k * 120, 0), (120, 130))
+            self.img_left.append(img)
+        
+        self.img_up = []
+        for l in range(23, 33):
+            img = sprite_sheet.subsurface((l * 120, 0), (120, 130))
+            self.img_up.append(img)
+        
+        self.img_right = []
+        for m in range(33, 44):
+            img = sprite_sheet.subsurface((m * 120, 0), (120, 130))
+            self.img_right.append(img)
+
+        self.action = 0 # 0: stoped 1:down 2:left 3:up 4:right
+        self.index_frame = 0
+        self.image = self.img_stoped[self.index_frame]
+        self.image = pygame.transform.scale(self.image, (120, 130))
         self.rect = self.image.get_rect()
-        self.rect.center = (300, 300)
+
+        self.x = 300
+        self.y = 300
+        self.rect.center = (self.x, self.y)
         self.andar_d = False
         self.andar_a = False
         self.andar_w = False
         self.andar_s = False
 
+        self.update_time = pygame.time.get_ticks()
+
+
     def update(self):
-        if self.andar_d == True:
-            if self.index_lista >= 42:
-                self.index_lista = 33
-                self.andar_d = False
-
-            self.index_lista += 0.1
-        
-        else:
-            if self.index_lista > 2:
-                self.index_lista = 0
-
-            self.index_lista += 0.025
-        self.image = self.imagem_poupy[int(self.index_lista)]
+        pass
 
     def andar_direita(self):
-        self.andar_d = True
-        self.index_lista = 33
+        pass
 
     def andar_esquerda(self):
         pass
